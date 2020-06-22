b8fd197a95bf3252984af22ab0edac66be8134c1
Ayush Chamoli <ayushchamoli8@gmail.com>
2020-06-22 17:19:58 +0530

    Added a custom parameter MSYS_MLT_PREFIX

--- 2-build-msys-cmake.sh
+++ 2-build-msys-cmake.sh
@@ -4,5 +4,5 @@ export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/opt/mlt-6.16.0/lib/pkgconfig"
 echo "${PKG_CONFIG_PATH}"
 ls -la /opt/mlt-6.16.0/lib/pkgconfig/
 mkdir -p build && cd build
-cmake -GNinja .. -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache
+cmake -GNinja .. -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DMSYS_MLT_PREFIX="C:/msys64"
 ninja
